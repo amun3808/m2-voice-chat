5ba31463873175082cd294d3c4d59839b4137872
Amun <amun.3808@gmail.com>
2025-04-10 12:48:02 +0300

    Don't need this anymore

--- public_implementation/client src/eterlib/NetStream.cpp
+++ public_implementation/client src/eterlib/NetStream.cpp
@@ -1,38 +1,3 @@
-// in bool CNetworkStream::__RecvInternalBuffer()
-
-// Find
-	if (m_recvBufOutputPos > 0)
-	{
-		int recvBufDataSize = m_recvBufInputPos - m_recvBufOutputPos;
-		if (recvBufDataSize > 0)
-		{
-			memmove(m_recvBuf, m_recvBuf + m_recvBufOutputPos, recvBufDataSize);
-		}
-
-		m_recvBufInputPos -= m_recvBufOutputPos;
-		m_recvBufOutputPos = 0;
-	}
-
-
-// Replace
-	if (m_recvBufOutputPos > 0)
-	{
-		if (m_recvBufInputPos > m_recvBufOutputPos)
-		{
-			int recvBufDataSize = m_recvBufInputPos - m_recvBufOutputPos;
-			memmove(m_recvBuf, m_recvBuf + m_recvBufOutputPos, recvBufDataSize);
-			m_recvBufInputPos -= m_recvBufOutputPos;
-		}
-		else
-		{
-			m_recvBufInputPos = 0;
-		}
-
-		m_recvBufOutputPos = 0;
-	}
-
-
-
 // find
 bool CNetworkStream::Send(int len, const void* pSrcBuf)
 {
